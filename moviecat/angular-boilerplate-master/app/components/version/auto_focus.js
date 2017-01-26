61bc9191465b2ccd5dd7db15b77bef864b0adf64
nancy <1138617421@qq.com>
2017-01-26 17:03:39 +0800

    修复首次打开焦点问题

--- moviecat/angular-boilerplate-master/app/components/version/auto_focus.js
+++ moviecat/angular-boilerplate-master/app/components/version/auto_focus.js
@@ -6,22 +6,29 @@
 (function (angular) {
     angular.module('moviecat.directives.auto_focus', [])
         .directive('autoFocus', ['$location', function ($location) {
-            var path = $location.path();
+            //var path = $location.path();
             //自定义指令
             return {
                 restrict: 'A',
                 link: function (scope, iEle) {
-                    var aLink=iEle.children().attr('href');
-                    var type = aLink.replace(/#(\/.+?)\/\d+/, '$1');
-                    console.log(type);
-                    if (path.startsWith(type)) {
-                        iEle.addClass('active');
-                    }
-                    iEle.on('click', function () {
-                        //先清除所有的
-                        iEle.parent().children().removeClass('active');
-                        iEle.addClass('active');
-                    })
+                    scope.$location = $location;
+                    //时刻的监视location.path()的变化
+                    scope.$watch('$location.path()', function (now) {
+                        var aLink = iEle.children().attr('href');
+                        var type = aLink.replace(/#(\/.+?)\/\d+/, '$1');
+                        // console.log(type);
+                        if (now.startsWith(type)) {
+                            iEle.parent().children().removeClass('active');
+                            iEle.addClass('active');
+                        }
+                    });
+
+
+                    //如果监视了url的变化的话，就没有必要再绑定点击事件了
+                    /*   iEle.on('click', function () {
+                     //先清除所有的
+                     iEle.addClass('active');
+                     })*/
 
                 }
             }
